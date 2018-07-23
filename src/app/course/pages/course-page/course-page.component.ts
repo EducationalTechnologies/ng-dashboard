40e5d73487b22662ff158291efe89af4f9d8920e
Daniel Biedermann <biedermann@dipf.de>
2018-07-23 10:58:55 +0200

    Remove ng-tla-service dependencies

--- src/app/course/pages/course-page/course-page.component.ts
+++ src/app/course/pages/course-page/course-page.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from 'ng-tla-services';
-import {CourseService} from '../../../core/services/course.service'
+import {CourseService} from '../../../core/services/course.service';
 import { select, Store } from '@ngrx/store';
 import * as fromCourses from '../../reducers';
 import * as CourseActions from '../../actions/course.actions';
@@ -17,8 +16,8 @@ import { Course } from '../../models/course';
 })
 export class CoursePageComponent implements OnInit {
 
-  private courses$ : Observable<Course[]>;
-  
+  private courses$: Observable<Course[]>;
+
   constructor(
     private store: Store<fromCourses.State>,
     ) { 
