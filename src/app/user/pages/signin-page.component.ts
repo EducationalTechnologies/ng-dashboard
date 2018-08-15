da4643c9f63ff77798eaf1fee926904942d5ddbe
Daniel Biedermann <daniel.biedermann@cmore-automotive.com>
2018-08-15 20:19:53 +0200

    sign in via call to store

--- src/app/user/pages/signin-page.component.ts
+++ src/app/user/pages/signin-page.component.ts
@@ -1,25 +1,31 @@
-import { Component, OnInit, isDevMode } from "@angular/core";
+import { Component, OnInit, OnDestroy, isDevMode } from "@angular/core";
 import {
   FormBuilder,
   FormGroup,
   FormControl,
   Validators
 } from "@angular/forms";
+import "rxjs/add/operator/takeWhile";
 import { UserService } from "../../core/services/user.service";
 import { environment } from "../../../environments/environment";
-
+import { isAuthenticated, State } from "../../reducers";
+import { Store } from "@ngrx/store";
+import { Router } from "../../../../node_modules/@angular/router";
 @Component({
   selector: "app-signin-page",
   templateUrl: "./signin-page.component.html",
   styleUrls: ["./signin-page.component.css"]
 })
-export class SignInPageComponent implements OnInit {
+export class SignInPageComponent implements OnDestroy, OnInit {
   loginForm: FormGroup;
   isSubmitting = false;
+  alive = true;
 
   constructor(
     private formBuilder: FormBuilder,
-    private userService: UserService
+    private userService: UserService,
+    private store: Store<State>,
+    private router: Router
   ) {
     this.loginForm = this.formBuilder.group({
       email: ["", Validators.required],
@@ -32,6 +38,17 @@ export class SignInPageComponent implements OnInit {
       this.loginForm.controls["email"].setValue("a@b.de");
       this.loginForm.controls["password"].setValue("1234");
     }
+
+    this.store.select(isAuthenticated)
+      .takeWhile(() => this.alive)
+      .subscribe(value => {
+        console.log("User authenticated, navigating to home");
+        this.router.navigate(["/courses"]);
+      });
+  }
+
+  public ngOnDestroy() {
+    this.alive = false;
   }
 
   submitForm() {
@@ -41,7 +58,9 @@ export class SignInPageComponent implements OnInit {
     const email = this.loginForm.controls["email"].value;
     const password = this.loginForm.controls["password"].value;
     this.userService.signIn(email, password).subscribe(
-      data => console.log("SIGN IN: ", data),
+      data => {
+        console.log("SIGN IN: ", data);
+      },
       err => {
         console.log("SIGN IN ERROR: ", err);
         this.isSubmitting = false;
